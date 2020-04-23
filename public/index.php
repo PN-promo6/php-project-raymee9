f4207d085ec166a35e943c8149751005ddc93961
Rémi <remi.olier9@gmail.com>
2020-04-23 11:17:48 +0200

    New version

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
-<!doctype html>
-<html lang="en">
+<html>
 
 <head>
     <!-- Required meta tags -->
@@ -7,17 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
     <title>DJSCOVER</title>
 </head>
+
 <header>
     <nav class="navbar sticky-top navbar-light" id="navTop">
         <a id="navPosition" class=" navbar-brand" href="#">
-            <img src="https://imagizer.imageshack.com/img923/4397/rVJmZj.png" width="50" height="50"
-                class="d-inline-block align-top clearfix" alt="">
+            <img src="https://imagizer.imageshack.com/img923/4397/rVJmZj.png" width="50" height="50" class="d-inline-block align-top clearfix" alt="">
             <h2 id="title">DJSCOVER</h2>
             <input id="searchBar" class="" type="text" placeholder="Search" aria-label="Search">
         </a>
@@ -34,12 +32,17 @@
                 <li class="">
                     <a class="nav-link" href="#">Master Class</a>
                 </li>
-                <li class="">
-                    <a class="nav-link" href="#">Link</a>
+                <li>
+                    <a class="nav-link" href="">Music</a>
                 </li>
-                <li class="">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
+                <ul>
+                    <li>
+                        <a class="nav-link" href="">House</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="">Techno</a>
+                    </li>
+                </ul>
             </ul>
         </div>
         <div class="row float-right " id="navSize">
@@ -61,74 +64,67 @@
         <section class="row" id="backPost">
             <div class="container">
                 <div class="card" id="cardPost">
-                    <h3>Title</h3>
-                    <img id="imgSize"
-                        src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
+                    <h3>Doremi - Exil</h3>
+                    <div><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/769221928&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                    </div>
+                    <p>Contenu du message</p>
+
+                    <p>Ajouter un commentaire</p>
+                </div>
+                <div class="card" id="cardPost">
+                    <h3>Doremi - Saxlife</h3>
+                    <div><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/668531609&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                    </div>
                     <p>Contenu du message</p>
 
                     <p>Ajouter un commentaire</p>
                 </div>
                 <div class="card" id="cardPost">
                     <h3>Title</h3>
-                    <img id="imgSize"
-                        src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
+                    <img id="imgSize" src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
                     <p>Contenu du message</p>
 
                     <p>Ajouter un commentaire</p>
                 </div>
                 <div class="card" id="cardPost">
                     <h3>Title</h3>
-                    <img id="imgSize"
-                        src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
+                    <img id="imgSize" src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
                     <p>Contenu du message</p>
 
                     <p>Ajouter un commentaire</p>
                 </div>
                 <div class="card" id="cardPost">
                     <h3>Title</h3>
-                    <img id="imgSize"
-                        src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
-                    <p>Contenu du message</p>
-
-                    <p>Ajouter un commentaire</p>
-                </div>
-                <div class="card" id="cardPost">
-                    <h3>Title</h3>
-                    <img id="imgSize"
-                        src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
+                    <img id="imgSize" src="https://www.tutomix.com/wp-content/uploads/2019/11/comment-etre-dj-debutant.jpg" alt="">
                     <p>Contenu du message</p>
 
                     <p>Ajouter un commentaire</p>
                 </div>
 
             </div>
-
+        </section>
 
     </div>
 
-    </section>
-
-
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 </body>
 
 <footer>
-    <ul>
-        <li>Contact Us</li>
-        <li>About</li>
-        <li>Create Account</li>
-    </ul>
+    <div>
+        <ul class="row">
+            <li class="col-1">Contact Us</li>
+            <li class="col-1">About</li>
+            <li class="col-1">Create Account</li>
+        </ul>
+    </div>
+
 </footer>
 
 </html>
